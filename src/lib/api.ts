ad2c6023f38242b2f688e7f3e3971d0acbd8070e
xdashutosh <ashuchaudhary6969@gmail.com>
2026-05-11 16:07:17 +0530

    map key changed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://ranneeti.org';
-const API_BASE_URL = 'http://localhost:5008/';
+const API_BASE_URL = 'https://ranneeti.org';
+// const API_BASE_URL = 'http://localhost:5008/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
